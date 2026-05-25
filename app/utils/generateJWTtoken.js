2752750c63256ab786a8bdc4f54e8634356dbabc
Arun Kumar Flutter <arun1818456@gmail.com>
2026-05-26 00:20:57 +0530

    token validate

--- app/utils/generateJWTtoken.js
+++ app/utils/generateJWTtoken.js
@@ -2,6 +2,6 @@ import jwt from "jsonwebtoken";
 
 export function jwtTokenGenerator(userId) {
     const payload = { user: { id: userId } };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "365d" });
     return token;
 }
